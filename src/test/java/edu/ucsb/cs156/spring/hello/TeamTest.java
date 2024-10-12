5094f0897180a2c8d7705e65f6a1e5ea98d02c45
neil-roy <neilroy@ucsb.edu>
2024-10-11 21:20:59 -0700

    finished mutation testing for team.java

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.Test;
 public class TeamTest {
 
     Team team;
+    Team team2;
 
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("test-team"); 
+        team2 = new Team("test-team2");
     }
 
     @Test
@@ -19,8 +21,34 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+   @Test
+   public void equals_returns_true() {
+        assert(team.equals(team)); 
+   }
+
+   @Test
+   public void equals_returns_false() {
+        assertEquals(false, team.equals(team2));
+   }
+
+   @Test
+   public void equals_not_instance_of_team() {
+    assertEquals(false, team.equals("not a team object"));
+   }
+
+   @Test 
+   public void hash_code_equals_true() {
+        assertEquals(team.hashCode(), team.hashCode());
+   }
+
+   @Test
+   public void hash_code_equals_false() {
+        assertEquals(false, team.hashCode() == team2.hashCode());
+   }
 
 }
